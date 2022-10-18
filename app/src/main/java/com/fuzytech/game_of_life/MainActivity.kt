eca070647a80d671213b1b0078b73d6f4cd94099
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2022-10-18 13:48:05 -0400

    init

---
+++ app/src/main/java/com/fuzytech/game_of_life/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fuzytech.game_of_life
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
